d5b7db88ad258a74c97f444522679c41f26cdfc8
The Dolls CNFT <130698249+TheDollsCardano@users.noreply.github.com>
2023-04-13 18:58:11 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'The Dolls · CNFT'
+copyright = '2023, The Dolls'
+author = 'Doc Team'
 
 release = '0.1'
 version = '0.1.0'
