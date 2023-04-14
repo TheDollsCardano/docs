d2b9f417a2d5398d22a3156c8c3ab798a7bb9179
The Dolls CNFT <130698249+TheDollsCardano@users.noreply.github.com>
2023-04-14 17:14:05 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-    'navigation_depth': 2,
     'titles_only': True,
 
 # -- Options for EPUB output
